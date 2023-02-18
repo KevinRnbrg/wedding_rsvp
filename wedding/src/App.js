3e95120dbe9b30e8f8c6984d99548bd9c6d14118
Kevin Rosenberg <90057334+KevinRnbrg@users.noreply.github.com>
2023-02-18 18:45:09 +0200

    Added invitation form page, added styles pages,
    Invitation form with route added, styles pages for both home and invitation form, downloaded mui materials

--- wedding/src/App.js
+++ wedding/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom"
 import Home from './pages/Home';
+import InvitationForm from './pages/InvitationForm';
  
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/invitation" element={<InvitationForm />} />
     </Routes>
   );
 }
---
+++ wedding/src/pages/InvitationForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import '../styles/App.css';
+import '../styles/FormStyles.css';
+
+const InvitationForm = () => {
+    return (
+        <Box className="App">
+            <Box className='outer-container'>
+                <Typography>Form</Typography>
+                
+            </Box>
+        </Box>
+    )
+
+}
+
+export default InvitationForm
