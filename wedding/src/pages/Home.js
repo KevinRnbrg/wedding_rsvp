e0dfb7eeeb0ffe3a8c4796a46b6c4533eebf5055
Kevin Rosenberg <90057334+KevinRnbrg@users.noreply.github.com>
2023-02-21 22:31:12 +0200

    Changed location of mapsq
    location of maps changed but still wrong

--- wedding/src/pages/Home.js
+++ wedding/src/pages/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
                 <iframe 
                     className="google-map"
                     title="googleMaps"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2022.1166106241444!2d26.180918116199397!3d59.54780910052754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4693a52b4502b5a1%3A0x6258049a6f7471e4!2sVihula%20M%C3%B5isa%20Viinamuuseum!5e0!3m2!1sen!2see!4v1676670407519!5m2!1sen!2see" 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d160911.08646495806!2d25.60196356099699!3d58.001181612026706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eb78625f8ab80f%3A0x3f56bf5d61e7681b!2sTaagepera%20Loss%20O%C3%9C!5e0!3m2!1sen!2see!4v1676749879603!5m2!1sen!2see" 
                     style={{ border: 0 }} 
                     allowfullscreen="" 
                     loading="lazy" 
