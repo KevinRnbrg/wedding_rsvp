9d4e71cf7248e6e81057f90b47828a38708ccab8
Kevin Rosenberg <kevinrosenberg04@gmail.com>
2023-02-22 14:13:03 +0200

    MUI materials, link to invitation, font styles, form info
    changed out some HTML to react MUI, changed to link to navigate to invitation form, added font styles from Google Fonts, added form questions with selections

--- wedding/src/pages/Home.js
+++ wedding/src/pages/Home.js
@@ -1,33 +1,43 @@
 import React from "react";
-import '../styles/App.css';
-import '../styles/HomeStyles.css';
+import { Box, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import "../styles/App.css";
+import "../styles/HomeStyles.css";
 
 const Home = () => {
-    return (
-        <div className="App">
-            <div className='outer-container'>
-                <h1>Kelli ja Tanel</h1>
-                <h2>15 juuni 2023</h2>
-                <button>Anna teada oma tulekust</button>
-                <div className="menu-buttons">
-                    <button className="menu-selection">Asukoht</button>
-                    <button className="menu-selection">Kingitused ja lilled</button>
-                    <button className="menu-selection">Kava</button>
-                    <button className="menu-selection">Riietumisstiil</button>
-                </div>
-                <p>Pulmad toimuvad suures mõisas kuskil Eestis. Sinna pääseb üldjuhul autoga, aga saab ka jalgsi või bussiga tulla. Parkimine ei ole võimalik ja auto tuleks koju jätta.</p>
-                <iframe 
-                    className="google-map"
-                    title="googleMaps"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d160911.08646495806!2d25.60196356099699!3d58.001181612026706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eb78625f8ab80f%3A0x3f56bf5d61e7681b!2sTaagepera%20Loss%20O%C3%9C!5e0!3m2!1sen!2see!4v1676749879603!5m2!1sen!2see" 
-                    style={{ border: 0 }} 
-                    allowfullscreen="" 
-                    loading="lazy" 
-                    referrerpolicy="no-referrer-when-downgrade" 
-                />
-            </div>
-        </div>
-    )
-}
+  return (
+    <Box className="App">
+      <Box className="outer-container">
+        <h1 className="font-h1">Kelli ja Tanel</h1>
+        <h2>15 juuni 2023</h2>
+        <Link to="/invitation" style={{ textDecoration: "none" }}>
+          <Button variant="outlined">Anna teada oma tulekust</Button>
+        </Link>
+        <Box className="menu-buttons">
+          <button className="menu-selection font-link">Asukoht</button>
+          <button className="menu-selection font-link">
+            Kingitused ja lilled
+          </button>
+          <button className="menu-selection font-link">Kava</button>
+          <button className="menu-selection font-link">Riietumisstiil</button>
+        </Box>
+        <Typography sx={{ mt: "2rem", px: "2rem" }}>
+          Pulmad toimuvad suures mõisas kuskil Eestis. Sinna pääseb üldjuhul
+          autoga, aga saab ka jalgsi või bussiga tulla. Parkimine ei ole
+          võimalik ja auto tuleks koju jätta.
+        </Typography>
+        <iframe
+          className="google-map"
+          title="googleMaps"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d160911.08646495806!2d25.60196356099699!3d58.001181612026706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eb78625f8ab80f%3A0x3f56bf5d61e7681b!2sTaagepera%20Loss%20O%C3%9C!5e0!3m2!1sen!2see!4v1676749879603!5m2!1sen!2see"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        />
+      </Box>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
--- wedding/src/pages/InvitationForm.js
+++ wedding/src/pages/InvitationForm.js
@@ -1,18 +1,72 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
-import '../styles/App.css';
-import '../styles/FormStyles.css';
+import React from "react";
+import {
+  Box,
+  Checkbox,
+  Typography,
+  TextField,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Button,
+} from "@mui/material";
+import "../styles/App.css";
+import "../styles/FormStyles.css";
 
 const InvitationForm = () => {
-    return (
-        <Box className="App">
-            <Box className='outer-container'>
-                <Typography>Form</Typography>
-                
-            </Box>
-        </Box>
-    )
+  return (
+    <Box className="App">
+      <Box className="outer-container">
+        <form /* onSubmit={handleSubmit} */>
+          <Typography>RSVP</Typography>
+          <Typography>1. Kirjuta oma ees- ja perekonnanimi:</Typography>
+          <TextField></TextField>
+          {/* checkboxes should be next to the question */}
+          <Typography>2. Kas sa võtad üritusest osa?</Typography>
+          <Checkbox />
+          <Typography>3. Kas sinuga tuleb kaasa +1?</Typography>
+          <Checkbox />
+          <Typography>4. Kas sa jääd ööseks?</Typography>
+          <Checkbox />
+          <Typography>5. Vali üks kolmest toidust õhtusöögiks:</Typography>
+          <RadioGroup aria-labelledby="food-choise" name="radio-buttons-group">
+            <FormControlLabel
+              value="karbonara"
+              control={<Radio />}
+              label="Karbonara"
+            />
+            <FormControlLabel
+              value="bolognese"
+              control={<Radio />}
+              label="Bolognese"
+            />
+            <FormControlLabel value="ramen" control={<Radio />} label="Ramen" />
+          </RadioGroup>
+          {/* if +1 is checked then offer to choose a food for them aswell */}
+          <Typography>
+            6. Vali üks kolmest toidust õhtusöögiks +1-le:
+          </Typography>
+          <RadioGroup aria-labelledby="food-choise" name="radio-buttons-group">
+            <FormControlLabel
+              value="karbonara"
+              control={<Radio />}
+              label="Karbonara"
+            />
+            <FormControlLabel
+              value="bolognese"
+              control={<Radio />}
+              label="Bolognese"
+            />
+            <FormControlLabel value="ramen" control={<Radio />} label="Ramen" />
+          </RadioGroup>
+          <Box className="save-button">
+            <Button variant="contained" type="submit">
+              Saada vastus
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Box>
+  );
+};
 
-}
-
-export default InvitationForm
+export default InvitationForm;
