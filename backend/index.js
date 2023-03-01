69f3aee74fda32d6306224b122f27480a6730bfb
Kevin Rosenberg <kevinrosenberg04@gmail.com>
2023-03-01 14:31:27 +0200

    Added index.js
    Added index.js file that connects to db

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const PORT = 8080;
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+app.use(morgan("dev"));
+
+{
+  /* link is wrong, must be changed when database is created */
+}
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fitnesstracker0.q4smdde.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("Database connection established"))
+  .catch((e) => console.error(e));
