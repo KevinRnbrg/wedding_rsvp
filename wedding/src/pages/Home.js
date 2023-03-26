d6d2865494304f9ee8b30508526099a80a58ae19
Kevin Rosenberg <kevinrosenberg04@gmail.com>
2023-03-26 11:05:58 +0300

    Switched RSVP link to a google forms

--- wedding/src/pages/Home.js
+++ wedding/src/pages/Home.js
@@ -9,7 +9,6 @@ import Schedule from "../components/Schedule";
 import Clothes from "../components/Clothes";
 
 const Home = () => {
-
   const [hidePlace, setHidePlace] = useState(false);
   const [hideGifts, setHideGifts] = useState(true);
   const [hideSchedule, setHideSchedule] = useState(true);
@@ -17,10 +16,8 @@ const Home = () => {
 
   function handlePlaceChange() {
     if (hidePlace === false) {
-
-    }
-    else {
-      setHidePlace(s => !s);
+    } else {
+      setHidePlace((s) => !s);
       setHideGifts(true);
       setHideSchedule(true);
       setHideClothes(true);
@@ -28,10 +25,8 @@ const Home = () => {
   }
   function handleGiftsChange() {
     if (hideGifts === false) {
-
-    }
-    else {
-      setHideGifts(s => !s)
+    } else {
+      setHideGifts((s) => !s);
       setHidePlace(true);
       setHideSchedule(true);
       setHideClothes(true);
@@ -39,10 +34,8 @@ const Home = () => {
   }
   function handleScheduleChange() {
     if (hideSchedule === false) {
-
-    }
-    else {
-      setHideSchedule(s => !s)
+    } else {
+      setHideSchedule((s) => !s);
       setHideGifts(true);
       setHidePlace(true);
       setHideClothes(true);
@@ -50,10 +43,8 @@ const Home = () => {
   }
   function handleClothesChange() {
     if (hideClothes === false) {
-
-    }
-    else {
-      setHideClothes(s => !s)
+    } else {
+      setHideClothes((s) => !s);
       setHideGifts(true);
       setHideSchedule(true);
       setHidePlace(true);
@@ -66,23 +57,62 @@ const Home = () => {
         <Box className="outer-container">
           <h1 className="font-h1">Kelli ja Tanel</h1>
           <h2 className="font-h2">15.06.2023</h2>
-          <Link to="/invitation" style={{ textDecoration: "none" }}>
-            <Button id="rsvp-button" variant="outlined">Anna teada oma tulekust (RSVP)</Button>
+          <Link
+            to="https://forms.gle/GUZyfHqSdP1mEVdEA"
+            style={{ textDecoration: "none" }}
+          >
+            {" "}
+            {/* to="/invitation" */}
+            <Button id="rsvp-button" variant="outlined">
+              Anna teada oma tulekust (RSVP)
+            </Button>
           </Link>
           <Box className="menu-buttons">
-            <button id="id-place" className="menu-selection font-link" onClick={() => {handlePlaceChange();}}>Asukoht</button>
-            <button id="id-gifts" className="menu-selection font-link" onClick={() => {handleGiftsChange();}}>
+            <button
+              id="id-place"
+              className="menu-selection font-link"
+              onClick={() => {
+                handlePlaceChange();
+              }}
+            >
+              Asukoht
+            </button>
+            <button
+              id="id-gifts"
+              className="menu-selection font-link"
+              onClick={() => {
+                handleGiftsChange();
+              }}
+            >
               Kingitused ja lilled
             </button>
-            <button id="id-schedule" className="menu-selection font-link" onClick={() => {handleScheduleChange();}}>Kava</button>
-            <button id="id-clothes" className="menu-selection font-link" onClick={() => {handleClothesChange();}}>Riietumisstiil</button>
+            <button
+              id="id-schedule"
+              className="menu-selection font-link"
+              onClick={() => {
+                handleScheduleChange();
+              }}
+            >
+              Kava
+            </button>
+            <button
+              id="id-clothes"
+              className="menu-selection font-link"
+              onClick={() => {
+                handleClothesChange();
+              }}
+            >
+              Riietumisstiil
+            </button>
           </Box>
-          <hr style={{
-            background: '#7AAF72' ,
-            color: '#7AAF72',
-            height: '3px',
-            width: '90%'
-          }}/>
+          <hr
+            style={{
+              background: "#7AAF72",
+              color: "#7AAF72",
+              height: "3px",
+              width: "90%",
+            }}
+          />
           {!hidePlace ? <Place /> : null}
           {!hideGifts ? <Gifts /> : null}
           {!hideClothes ? <Clothes /> : null}
