d2c60f39409262189f0372be4c4854a72925d9c2
Kevin Rosenberg <kevinrosenberg04@gmail.com>
2023-03-31 16:51:41 +0300

    Small typo fix

--- wedding/src/components/Clothes.js
+++ wedding/src/components/Clothes.js
@@ -4,12 +4,10 @@ function Clothes() {
   return (
     <Box className="component-box">
       <Box className="light-box">
-        <p className="font-default">
-          Palume kanda pidulikku riietust.
-        </p>
+        <p className="font-default">Palume kanda pidulikku riietust.</p>
         <p className="font-default">
           Järgmiseks hommikuks võtke kaasa mugavad riided ja kiiremad saavad
-          järgmisel hommikul pidustutest taastuda mõisa hubases spaas.
+          järgmisel hommikul pidustustest taastuda mõisa hubases spaas.
         </p>
         <p className="font-default">
           PS! Tseremoonia toimub plaani järgi mõisahoovis ning järgnevad
